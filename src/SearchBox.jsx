e167a941295962a5ce44c02a0aa3ed9c1c41ae83
Onkar-Mane-3333 <maneonkar67@gmail.com>
2026-01-13 11:18:10 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}) {
     let [city,setCity] = useState("");
     let [error,setError] = useState(false);
     const API_URL ="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY ="c9a4a6a5cb13bbb7648a69b3c304ae23";
+    const API_KEY =""; //use your key here from the openweathermap
 
     let getWeatherInfo = async () => {
         try{
