9a37af394e64b613c90bb7ac45eb65c704ca6eab
Onkar-Mane-3333 <maneonkar67@gmail.com>
2026-01-14 16:52:01 +0530

    Secure API key using env file and update config

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}) {
     let [city,setCity] = useState("");
     let [error,setError] = useState(false);
     const API_URL ="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY ="c9a4a6a5cb13bbb7648a69b3c304ae23";
+    const API_KEY =import.meta.env.VITE_OPENWEATHER_API_KEY;
 
     let getWeatherInfo = async () => {
         try{
